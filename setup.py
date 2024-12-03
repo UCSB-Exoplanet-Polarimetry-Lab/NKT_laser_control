ab15f54b18bd4b5025867dc7e5bc73eddac6dde1
William Melby <wcmelby@ucsb.edu>
2024-12-03 13:49:01 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nkt_controls',  # Replace with your package name
+    name='NKT_laser_control',  # Replace with your package name
     version='0.1.0',  # Replace with your package version
     author='William Melby',  # Replace with your name
     author_email='wcmelby@ucsb.edu',  # Replace with your email
-    description='Python controls for NKT Photonics components: SuperK SELECT, COMPACT, and RF Driver.',
-    packages=find_packages(),  # Automatically find packages in the current directory
-    install_requires=[
-        # List your package dependencies here
-        # 'some_package>=1.0.0',
-    ],
+    description='Python controls for NKT Photonics components.',
+    packages=find_packages(include=['nkt_controls', 'nkt_controls.*']),
+    # install_requires=[
+    #     # List your package dependencies here
+    #     # 'some_package>=1.0.0',
+    # ],
     # classifiers=[
     #     'Programming Language :: Python :: 3',
     #     'License :: OSI Approved :: MIT License',
